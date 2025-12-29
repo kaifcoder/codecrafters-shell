5218f27e08bf40d540645028c3b3ae4ac1f0b9b8
kaifcoder <kaifmohd2014@gmail.com>
2025-12-29 17:49:28 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 import sys
 
+def echo(message: str) -> None:
+    sys.stdout.write(message + "\n")
 
 def main():
     sys.stdout.write("$ ")
     input_string = input()
     exit(0) if input_string == "exit" else None
-    sys.stdout.write(f"{input_string}: command not found\n")
+    if input_string.startswith("echo "):
+        echo(input_string[5:])
+    else:
+        sys.stdout.write(f"{input_string}: command not found\n")
 
 
 if __name__ == "__main__":
